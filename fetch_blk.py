f94b097aa175202f627a2263b16fc55fcde8f0fa
millecodex <jeffnijsse@gmail.com>
2024-05-11 18:16:26 +1200

    some error handling

--- fetch_blk.py
+++ fetch_blk.py
@@ -2,11 +2,19 @@ import requests
 import json
 import os
 import time
+from urllib.parse import urlencode, urlunparse
 
 # env API key
 api_key = os.getenv('ETHERSCAN_API_KEY')
 
-def fetch_tx(api_key, start_block, end_block, chunk_size):
+# directory for output
+directory = 'data'
+
+# Check if the directory exists and create it if necessary
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def fetch_tx(start_block, end_block, chunk_size):
     # Initialize timer
     start = time.time()
     
@@ -15,16 +23,26 @@ def fetch_tx(api_key, start_block, end_block, chunk_size):
         chunk_end = min(chunk_start + chunk_size - 1, end_block)
         
         for block in range(chunk_start, chunk_end + 1):
-            url = f"https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag={hex(block)}&boolean=true&apikey={api_key}"
-            response = requests.get(url)
-            
-            if response.status_code == 200:
+            query_params = {
+                'module': 'proxy',
+                'action': 'eth_getBlockByNumber',
+                'tag': hex(block),
+                'boolean': 'true',
+                'apikey': api_key
+            }
+            url_parts = ('https', 'api.etherscan.io', '/api', '', urlencode(query_params), '')
+            url = urlunparse(url_parts)
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
                 block_data = response.json()
-                if block_data['result']:  # Make sure the result key exists and has data
-                    transactions.extend(block_data['result']['transactions'])
-        
-        # write out location
-        directory = 'data' # make sure it exists
+                if 'result' in block_data and isinstance(block_data['result'], dict):
+                    transactions.extend(block_data['result'].get('transactions', []))
+            except requests.RequestException as e:
+                print(f"Request error: {e}")
+            except json.JSONDecodeError:
+                print("Failed to decode JSON from response.")
+            
         filename = f'{directory}/blk_{chunk_start}_{chunk_end}.json'  # Adjust the file path
         # Write the transactions to a file with pretty printing
         with open(filename, 'w') as file:
@@ -39,13 +57,12 @@ def fetch_tx(api_key, start_block, end_block, chunk_size):
     
     print(f"Total time taken: {total_time:.2f} seconds")
     print(f"Average time per block: {average_time:.2f} seconds")
-    
+
 
 # params
 start_block = 19800000  # integer block number
-end_block =   19800010    # integer block number
+end_block =   19800004    # integer block number
 chunk_size = 5
 
 # Fetch transactions
-fetch_tx(api_key, start_block, end_block, chunk_size)
-
+fetch_tx(start_block, end_block, chunk_size)
